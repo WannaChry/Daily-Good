350f01097a9714274f2d00277c15dfb9f428b004
leopard-010 <laisleoaprd@gmail.com>
2025-09-13 13:03:58 +0200

    13.09 - Tipps zu Firesore hinzugefügt

--- lib/pages/models/tipp.dart
+++ lib/pages/models/tipp.dart
@@ -1,16 +1,13 @@
 class Tipp {
-  final String category;
   final String message;
 
-  Tipp({required this.category, required this.message});
+  Tipp({required this.message});
 
   Map<String, dynamic> toJson() => {
-    'category': category,
     'message': message,
   };
 
   factory Tipp.fromJson(Map<String, dynamic> json) => Tipp(
-    category: json['category'],
     message: json['message'],
   );
 }
