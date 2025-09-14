e4d9b81d92a4e7ddccb2c44ab2917e51185de7b3
leopard-010 <laisleoaprd@gmail.com>
2025-09-14 15:36:45 +0200

    14.9 - Json vorbereitet und in FIrestore geladen - Tipps werden nur noch aus Firestore geladen

--- lib/pages/models/tipp.dart
+++ lib/pages/models/tipp.dart
@@ -1,13 +1,16 @@
 class Tipp {
+  String id;
   final String message;
 
-  Tipp({required this.message});
+  Tipp({required this.id, required this.message});
 
   Map<String, dynamic> toJson() => {
+    'id': id,
     'message': message,
   };
 
   factory Tipp.fromJson(Map<String, dynamic> json) => Tipp(
+    id: json['id'],
     message: json['message'],
   );
 }
